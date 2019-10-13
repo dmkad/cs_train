f951ee4673f67aa1965e4f8d8690af1f430b078a
kadoshnikov dmitry <unattainablesky@gmail.com>
2019-10-13 11:49:40 +0400

    Подготовка к работе с циклами и коллекциями

--- addressbook_wt/addressbook_wt/tests/GroupCreationTests.cs
+++ addressbook_wt/addressbook_wt/tests/GroupCreationTests.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace WebAddressbookTests
@@ -17,7 +18,12 @@ namespace WebAddressbookTests
             group.Header = "group2";
             group.Footer = "group3";
 
+            List<GroupData> oldgroups = app.Groups.GetGroupList();
+
             app.Groups.Create(group);
+
+            List<GroupData> newgroups = app.Groups.GetGroupList();
+            Assert.AreEqual(oldgroups.Count + 1, newgroups.Count);
         }
 
         [Test]
@@ -27,7 +33,47 @@ namespace WebAddressbookTests
             group.Header = "";
             group.Footer = "";
 
+            List<GroupData> oldgroups = app.Groups.GetGroupList();
+
             app.Groups.Create(group);
+
+            List<GroupData> newgroups = app.Groups.GetGroupList();
+            Assert.AreEqual(oldgroups.Count + 1, newgroups.Count);
+        }
+
+        [Test]
+        public void BadNameGroupCreationTest()
+        {
+            GroupData group = new GroupData("a'a");
+            group.Header = "";
+            group.Footer = "";
+
+            List<GroupData> oldgroups = app.Groups.GetGroupList();
+
+            app.Groups.Create(group);
+
+            List<GroupData> newgroups = app.Groups.GetGroupList();
+            Assert.AreNotEqual(
+                oldgroups.Count + 1, newgroups.Count);
+        }
+
+        [Test]
+        public void TenGroupCreationTest()
+        {
+            int i = 0;
+            while (i != 10)
+            {
+                GroupData group = new GroupData("tengroup " + i);
+                group.Header = "tengroup" + i;
+                group.Footer = "tengroup" + i;
+                List<GroupData> oldgroups = app.Groups.GetGroupList();
+
+                app.Groups.Create(group);
+
+                List<GroupData> newgroups = app.Groups.GetGroupList();
+                Assert.AreEqual(oldgroups.Count + 1, newgroups.Count);
+                i++;
+            }
         }
     }
 }
